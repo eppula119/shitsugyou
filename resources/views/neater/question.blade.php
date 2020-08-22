5a99d1aff0ef75304700e9ef7a51121babe93436
kousuke <firstdouga@gmail.com>
2020-08-22 11:21:24 +0900

    質問画面CSSコーディング一通り完了

--- resources/views/neater/question.blade.php
+++ resources/views/neater/question.blade.php
@@ -13,7 +13,7 @@
 
   <script src="https://kit.fontawesome.com/39ab84bfc8.js" crossorigin="anonymous"></script>
   <link href="https://fonts.googleapis.com/css?family=Noto+Serif+JP:400,700&display=swap" rel="stylesheet">
-  <!-- <link href="{{ asset('css/app.css') }}" rel="stylesheet"> -->
+  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
 </head>
 
@@ -23,7 +23,7 @@
   <div class="p-headerItem">
     <div class="p-headerItem__logo">
       <a href="" class="p-topLink">
-        <img src="#" alt="" class="p-topLink__img">
+        <img src="/images/panda.png"  alt="" class="p-topLink__img">
       </a>
     </div>
     <div class="p-headerItem__menu">
@@ -54,17 +54,17 @@
 
 <main class="l-main">
   <div class="p-questWrap">
-    <img src="" alt="" class="p-questWrap__img p-neaterImg">
+    <img src="/images/panda.png" style="width: 80px"  alt="" class="p-questWrap__img p-neaterImg">
     <div class="p-questWrap__content c-comment">
       <p class="p-questComment">
         1.1年以上働いた？
       </p>
     </div>
     <form action="" class="p-answerForm">
-      <button class="p-quest1 c-btn">働いてた</button>
-      <button class="p-quest2 c-btn">働いてない</button>
-      <button class="p-quest3 c-btn">多分働いてた</button>
-      <button class="p-quest4 c-btn">分からない</button>
+      <button class="p-answerForm__btn c-btn">働いてた</button>
+      <button class="p-answerForm__btn c-btn">働いてない</button>
+      <button class="p-answerForm__btn c-btn">多分働いてた</button>
+      <button class="p-answerForm__btn c-btn">分からない</button>
     </form>
     <a href="" class="p-questWrap__back"><span class="p-backTxt">戻る</span></a>
 
