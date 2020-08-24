0a7d65d276e8f78f8cce2130df05dcb540e1ce62
kousuke <firstdouga@gmail.com>
2020-08-24 14:44:20 +0900

    掲示板CSSコーディング一通り完了

--- resources/views/neater/bord.blade.php
+++ resources/views/neater/bord.blade.php
@@ -13,53 +13,46 @@
 
   <script src="https://kit.fontawesome.com/39ab84bfc8.js" crossorigin="anonymous"></script>
   <link href="https://fonts.googleapis.com/css?family=Noto+Serif+JP:400,700&display=swap" rel="stylesheet">
-  <!-- <link href="{{ asset('css/app.css') }}" rel="stylesheet"> -->
+  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
 </head>
 
 <body>
 <!------------------------------------- ヘッダー ----------------------------------------------->
-<header class="l-header">
+<header class="l-header p-header">
   <div class="p-headerItem">
     <div class="p-headerItem__logo">
       <a href="" class="p-topLink">
-        <img src="#" alt="" class="p-topLink__img">
+        <img src="images/panda.png" style="width: 80px;" alt="" class="p-topLink__img">
       </a>
     </div>
     <div class="p-headerItem__menu">
       <button class="p-menuBtn">メニュー</button>
       <div class="p-headerMenu"> 
-        <ul>
-          <li class="p-headerMenu__list">
-            <ul class="p-menuList">
-              <li class="p-menuList__item">マイページ</li>
-              <li class="p-menuList__item">アイディ投稿</li>
-              <li class="p-menuList__item">気になるリスト</li>
-              <li class="p-menuList__item">
-                <form action="" method="POST">
-                  @csrf
-                  <button type="submit" class="p-logout c-btn"></button>
-                </form>
-              </li>
-            </ul>
+        <ul class="p-menuList">
+          <li class="p-menuList__item">マイページ</li>
+          <li class="p-menuList__item">アイディ投稿</li>
+          <li class="p-menuList__item">気になるリスト</li>
+          <li class="p-menuList__item">
+            <form action="" method="POST">
+              @csrf
+              <button type="submit" class="c-logout c-btn" >ログアウト</button>
+            </form>
           </li>
         </ul>
       </div>
-      <p class="p-categorySearch"><i class="far fa-list-alt"></i>カテゴリーから探す</p>
-      <div class="p-categoryMenu"> 
-        <ul>
-          <li class="p-categoryMenu__list">
-            <ul class="p-menuList">
-              <li class="p-menuList__item">失業給付</li>
-              <li class="p-menuList__item">お金</li>
-              <li class="p-menuList__item">ハローワーク</li>
-              <li class="p-menuList__item">手続方法</li>
-              <li class="p-menuList__item">持ち物</li>
-            </ul>
-          </li>
-        </ul>
-      </div>
+      
     </div>
+    <p class="p-categorySearch"><i class="far fa-list-alt"></i>カテゴリーから探す</p>
+      <div class="p-categoryMenu"> 
+          <ul class="p-menuList">
+            <li class="p-menuList__item">失業給付</li>
+            <li class="p-menuList__item">お金</li>
+            <li class="p-menuList__item">ハローワーク</li>
+            <li class="p-menuList__item">手続方法</li>
+            <li class="p-menuList__item">持ち物</li>
+          </ul>      
+      </div>
   </div>
 </header>
 
@@ -71,19 +64,19 @@
       <div class="p-bordItem">
         <span class="p-bordItem__category c-category">失業手当</span>
         <span class="p-bordItem__text">なんでお金は減ってくの？</span>
-        <span class="p-bordItem__name">あゆ</span><i class="far fa-comment p-bordItem__icon"></i><span class="p-bordItem__total">5</span>
+        <span class="p-bordItem__name">あゆ</span><i class="far fa-comment p-bordItem__icon"><span class="p-bordTotal">5</span></i>
       </div>
 
       <div class="p-bordItem">
         <span class="p-bordItem__category c-category">失業手当</span>
         <span class="p-bordItem__text">なんでお金は減ってくの？</span>
-        <span class="p-bordItem__name">あゆ</span><i class="far fa-comment p-bordItem__icon"></i><span class="p-bordItem__total">5</span>
+        <span class="p-bordItem__name">あゆ</span><i class="far fa-comment p-bordItem__icon"><span class="p-bordTotal">5</span></i>
       </div>
 
       <div class="p-bordItem">
         <span class="p-bordItem__category c-category">失業手当</span>
         <span class="p-bordItem__text">なんでお金は減ってくの？</span>
-        <span class="p-bordItem__name">あゆ</span><i class="far fa-comment p-bordItem__icon"></i><span class="p-bordItem__total">5</span>
+        <span class="p-bordItem__name">あゆ</span><i class="far fa-comment p-bordItem__icon"><span class="p-bordTotal">5</span></i>
       </div>
 　<!--ーーーーーーーーーーーーーーーーーー ページネーション ーーーーーーーーーーーーーーーーーー-->
       <ul class="c-pagenation p-bordPage">
